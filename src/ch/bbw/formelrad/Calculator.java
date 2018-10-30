07b54df3902752c0371176ce6c44340561357265
Davide Mastruzzo <Davide.Mastruzzo@netcetera.com>
2018-10-30 12:13:24 +0100

    code cleanup

--- src/ch/bbw/formelrad/Calculator.java
+++ src/ch/bbw/formelrad/Calculator.java
@@ -2,66 +2,67 @@ package ch.bbw.formelrad;
 
 /**
  * Berechnet das Formelrad
+ *
  * @author Peter Rutschmann
  * @version 13.09.2018
  */
 public class Calculator {
-	private double leistung;
-	private double spannung;
-	private double strom;
-	private double widerstand;
-	
-	public Calculator(double leistung, double spannung, double strom, double widerstand) {
-		super();
-		this.leistung = leistung;
-		this.spannung = spannung;
-		this.strom = strom;
-		this.widerstand = widerstand;
-	}
-	
-	public double getLeistung() {
-		return leistung;
-	}
-	
-	public double getSpannung() {
-		return spannung;
-	}
+    private double leistung;
+    private double spannung;
+    private double strom;
+    private double widerstand;
 
-	public double getStrom() {
-		return strom;
-	}
+    public Calculator(double leistung, double spannung, double strom, double widerstand) {
+        super();
+        this.leistung = leistung;
+        this.spannung = spannung;
+        this.strom = strom;
+        this.widerstand = widerstand;
+    }
 
-	public double getWiderstand() {
-		return widerstand;
-	}
+    public double getLeistung() {
+        return leistung;
+    }
 
-	@Override
-	public String toString() {
-		return "Calculator [leistung=" + leistung + 
-				", spannung=" + spannung + 
-				", strom=" + strom + 
-				", widerstand="	+ widerstand + "]";
-	}
+    public double getSpannung() {
+        return spannung;
+    }
 
-	public void calculate() {
-		/* Hier auf Grund der vorhanden Werte entscheiden
-		 * welche Methode unten aufgerufen werden muss.
-		 */
-	}
+    public double getStrom() {
+        return strom;
+    }
 
-	public double pFromUAndI(double u, double i){
-		return u*i;
-	}
+    public double getWiderstand() {
+        return widerstand;
+    }
 
-	public double pFromUAndR(double u, double r){
-		return Math.pow(u,2)/r;
-	}
+    @Override
+    public String toString() {
+        return "Calculator [leistung=" + leistung +
+                ", spannung=" + spannung +
+                ", strom=" + strom +
+                ", widerstand=" + widerstand + "]";
+    }
 
-	public double pFromRAndI(double r, double i){
-		return r*Math.pow(i,2);
-	}
+    public void calculate() {
+        /* Hier auf Grund der vorhanden Werte entscheiden
+         * welche Methode unten aufgerufen werden muss.
+         */
+    }
 
-	public double uFromRandI(double r, double i){
-		return r*i;
-	}
+    public double pFromUAndI(double u, double i) {
+        return u * i;
+    }
+
+    public double pFromUAndR(double u, double r) {
+        return Math.pow(u, 2) / r;
+    }
+
+    public double pFromRAndI(double r, double i) {
+        return r * Math.pow(i, 2);
+    }
+
+    public double uFromRandI(double r, double i) {
+        return r * i;
+    }
 }
